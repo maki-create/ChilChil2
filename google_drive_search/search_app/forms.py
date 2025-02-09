ae1e6f1de4fccb3579c64e1d4264cdaef85aeb91
maki-create <makimai.08170217@gmail.com>
2025-02-09 18:55:41 +0900

    Delete google_drive_search directory

--- google_drive_search/search_app/forms.py
+++
@@ -1,4 +0,0 @@
-from django import forms
-
-class SearchForm(forms.Form):
-    keyword = forms.CharField(label="検索キーワード", max_length=100)
--- google_drive_search/search_app/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import search_files
-
-urlpatterns = [
-    path('', search_files, name='search'),
-]
