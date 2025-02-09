694430d8974ba18992c23f4b0550dbeac6d32285
maki-create <makimai.08170217@gmail.com>
2025-02-09 18:40:24 +0900

    Update ESTJ.py

--- pages/ESTJ.py
+++ pages/ESTJ.py
@@ -1,9 +1,12 @@
 import streamlit as st
 
+st.markdown("""
+    <style>
+        section[data-testid="stSidebar"] {display: none;}
+    </style>
+    """, unsafe_allow_html=True)
+
+
 st.title("診断結果: ESTJ")
 
 st.write("あなたは戦略的な思考を持ち、長期的な視野で物事を計画するタイプです。")
-
-# ホームに戻るボタン
-if st.button("ホームに戻る"):
-    st.switch_page("Home")  # `app.py` に戻る
