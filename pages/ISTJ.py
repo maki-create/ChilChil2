d76164f62fa935356f2526382c7c12c99f4871de
maki-create <makimai.08170217@gmail.com>
2025-02-10 10:47:25 +0900

    Update ISTJ.py

--- pages/ISTJ.py
+++ pages/ISTJ.py
@@ -11,6 +11,6 @@ st.title("診断結果: ISTJ")
 
 st.write("あなたは戦略的な思考を持ち、長期的な視野で物事を計画するタイプです。")
 
-# 「元のアプリに戻る」ボタン
-if st.button("元のページに戻る"):
-    st.switch_page("app.py")  
+# ホームに戻るボタン
+if st.button("ホームに戻る"):
+    st.switch_page("app.py")  # `app.py` に戻る
