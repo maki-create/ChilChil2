a9c24f9966c86e2d66a5b2242fb02e10bf9fb649
maki-create <makimai.08170217@gmail.com>
2025-02-10 10:47:05 +0900

    Update ISFP.py

--- pages/ISFP.py
+++ pages/ISFP.py
@@ -6,7 +6,10 @@ st.markdown("""
     </style>
     """, unsafe_allow_html=True)
 
-
 st.title("診断結果: ISFP")
 
 st.write("あなたは戦略的な思考を持ち、長期的な視野で物事を計画するタイプです。")
+
+# ホームに戻るボタン
+if st.button("ホームに戻る"):
+    st.switch_page("app.py")  # `app.py` に戻る
