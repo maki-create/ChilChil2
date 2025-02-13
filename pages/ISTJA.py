2b01cb950492213aa825e400a71e2a06d20b56bc
maki-create <makimai.08170217@gmail.com>
2025-02-13 17:44:19 +0900

    Add files via upload

---
+++ pages/ISTJA.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.markdown("""
+    <style>
+        section[data-testid="stSidebar"] {display: none;}
+    </style>
+    """, unsafe_allow_html=True)
+
+
+st.title("診断結果: ISTJA")
+
+st.write("あなたは戦略的な思考を持ち、長期的な視野で物事を計画するタイプです。")
+
+# 「元のアプリに戻る」ボタン
+if st.button("元のページに戻る"):
+    st.switch_page("app.py")  
