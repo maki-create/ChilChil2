267fc0a4f7826c9c3bed244b9b77cc868bd8d9ca
maki-create <makimai.08170217@gmail.com>
2025-03-13 18:30:29 +0900

    Update free_questions.py

--- free_questions.py
+++ free_questions.py
@@ -1,1 +1,11 @@
+import streamlit as st
+
+st.title("自由記述の質問")
+
+q1 = st.text_area("あなたが一番影響を受けた作品は何ですか？")
+q2 = st.text_area("推しのどんなところが好きですか？")
+q3 = st.text_area("推し活の中で最も印象に残っているエピソードを教えてください。")
+
+if st.button("診断結果を表示"):
+    st.switch_page("result")
 
