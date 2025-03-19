f838c5dccb092edc2208776351570399e92ef2ba
maki-create <makimai.08170217@gmail.com>
2025-03-19 20:10:52 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from datetime import datetime
-import pytz
+
 
 st.markdown("""
     <style>
