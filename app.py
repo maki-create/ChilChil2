35ae6b857c5f81871160f86b26d8ffcab433e5fd
maki-create <makimai.08170217@gmail.com>
2025-03-23 12:11:40 +0900

    Update app.py

--- app.py
+++ app.py
@@ -143,7 +143,7 @@ def diagnosis_page():
         )
         st.session_state["final_result"] = final_result
         st.session_state["result_page"] = True
-        st.experimental_rerun()
+        st.rerun()
 
         
         
