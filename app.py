b380bca26005269f0d06c0eb8b119e8360643c3d
maki-create <makimai.08170217@gmail.com>
2025-03-23 12:07:51 +0900

    Update app.py

--- app.py
+++ app.py
@@ -152,7 +152,7 @@ def diagnosis_page():
 
 # メイン処理
 def main():
-    if st.session_state.get("result_page", Fales):
+    if st.session_state.get("result_page", True):
         result_page()
     else:
         diagnosis_page()
