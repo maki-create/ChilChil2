eb0fb4ecd2c80de209eb74c6c40515991a562294
maki-create <makimai.08170217@gmail.com>
2025-04-03 14:36:04 +0900

    Update app.py

--- app.py
+++ app.py
@@ -182,11 +182,11 @@ for category, questions in categories.items():
         # 名前が入力されていない場合、エラーを表示して中断
         if not st.session_state["name"]:
             st.error("お名前を入力してください。")
-            return
+            st.stop()
             
 if len(responses) < 36:
         st.error("全ての質問に回答してください")
-        return
+        st.stop()
         
 
         final_result = (
