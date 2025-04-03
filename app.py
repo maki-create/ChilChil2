b4e2fbcb95888f7b1cbe4cf866428c7311e5b279
maki-create <makimai.08170217@gmail.com>
2025-04-03 13:49:04 +0900

    Update app.py

--- app.py
+++ app.py
@@ -184,8 +184,8 @@ for category, questions in categories.items():
             st.error("お名前を入力してください。")
 return
             
-        if len(responses) < 36:
-            st.error("全ての質問に回答してください")
+    if len(responses) < 36:
+        st.error("全ての質問に回答してください")
         return
 
         final_result = (
